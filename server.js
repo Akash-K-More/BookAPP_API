110e23d60a60462ded4ce8fd43b79b77a2e552f6
= <=>
2024-11-15 06:34:03 +0530

    cors error

--- server.js
+++ server.js
@@ -11,19 +11,8 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-
-const corsOptions = {
-    origin: ['http://localhost:3000', 'https://your-production-frontend-domain.com'], // replace with your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 // Enable CORS for all origins or specify certain origins
 app.use(cors());
-
-
-
 
 // // If you want to restrict CORS to specific origin(s)
 // app.use(cors({
